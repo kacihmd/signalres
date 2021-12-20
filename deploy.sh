1e853775ce591e66118102b3035dbb03b6eb72a6
kacihmd <kaci.hmd76@gmail.com>
2021-12-20 13:04:45 +0100

    Modification de deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+# Connexion au compte root
+su < rotomagus 
+
+# Installation des paquets nécéssaires
 apt install git
+
+# Récupération du projet
+cd /var/www/
+git pull https://github.com/kacihmd/signalres

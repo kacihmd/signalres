f5829885507da81cd8c8d42b08148f0bcac5129f
urouen <urouen@debian-9.6>
2021-12-21 21:54:29 +0100

    Avancement sur MainView

--- views/MainView.php
+++ views/MainView.php
@@ -1,3 +1,70 @@
 <!-- A définir ici le modèle de page générale -->
 <!-- Par exemple le menu qui s'affiche partout, le footer etc... -->
 
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title><?= $title ?></title>
+        <link href="css/css.css" rel="stylesheet" /> 
+    </head>
+        
+    <header>
+        <div class="inner">
+            <div class="logo_container"> 
+                <img src="images/logo.png">
+            </div> 
+        </div>
+    </header>
+
+    <body>
+        <?= $content ?>
+        a
+</br>
+a
+</br> a
+</br> a
+</br> a
+</br> a
+</br> a
+</br> a
+</br> a
+</br> a
+</br> a
+</br> a
+</br> a
+</br> a
+</br> a
+</br> a
+</br> a
+</br> a
+</br> a
+</br> a
+</br> a
+</br> a
+</br> a
+</br> a
+</br> a
+</br> a
+</br> a
+</br> a
+</br> a
+</br> a
+</br> a
+</br> a
+</br> a
+</br> a
+</br> a
+</br> a
+</br> a
+</br> a
+</br> a
+</br> a
+</br>
+    </body>
+
+    <footer> 
+
+    </footer>
+
+</html>
